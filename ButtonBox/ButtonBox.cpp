79b37f0d3de63063bdc72a8f0f4e78977288d962
opcode2 <shane.warren@gmail.com>
2013-08-02 19:13:06 -0500

    First Project Checkin

---
+++ ButtonBox/ButtonBox.cpp
@@ -0,0 +1,14 @@
+// Do not remove the include below
+#include "ButtonBox.h"
+
+//The setup function is called once at startup of the sketch
+void setup()
+{
+
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+
+}
